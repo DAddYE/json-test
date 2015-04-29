7a85b6de2dbc4e893e785c2260abb451fd4d979e
Davide D'Agostino <info@daddye.it>
2015-04-29 14:59:59 -0700

    Added buffered codec test

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ### Results
 
-	BenchmarkCodec	       1	2461422589 ns/op	 6400256 B/op	  400017 allocs/op
+	BenchmarkCodec	       1	2445461140 ns/op	 6400256 B/op	  400017 allocs/op
+	BenchmarkCodecBuffer	2000000000	         0.05 ns/op	       0 B/op	       0 allocs/op
 	BenchmarkStdLib	2000000000	         0.07 ns/op	       0 B/op	       0 allocs/op
--- message_test.go
+++ message_test.go
@@ -1,6 +1,7 @@
 package json_bench
 
 import (
+	"bufio"
 	"encoding/json"
 	"io"
 	"os"
@@ -19,6 +20,34 @@ func BenchmarkCodec(b *testing.B) {
 
 	var h codec.JsonHandle
 	dec := codec.NewDecoder(f, &h)
+
+	b.ResetTimer()
+	var i int
+	for ; ; i++ {
+		var m Message
+		if err := dec.Decode(&m); err == io.EOF {
+			break
+		} else if err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	if i != Messages {
+		b.Fatalf("expected %d messages got %d", Messages, i)
+	}
+}
+func BenchmarkCodecBuffer(b *testing.B) {
+	b.ReportAllocs()
+
+	f, err := os.Open("./testdata/data.json")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	buf := bufio.NewReader(f)
+
+	var h codec.JsonHandle
+	dec := codec.NewDecoder(buf, &h)
 
 	b.ResetTimer()
 	var i int
